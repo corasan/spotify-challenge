1567b6ae19c18369ef88b2a1df2a53ce7f594f22
Henry Paulino <henrypl360@gmail.com>
2017-03-19 20:21:47 -0400

    Add get and put requests

--- server.js
+++ server.js
@@ -5,9 +5,49 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(morgan('dev'))
 app.use(express.static(`${__dirname}/public`))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// MongoDB user and password
+const dbUser = process.env.MONGODB_USER
+const dbPassword = process.env.MONGODB_PASS
+
+const peopleSchema = mongoose.Schema({
+  id: mongoose.Schema.ObjectId,
+  name: String,
+  favoriteCity: String,
+})
+const People = mongoose.model('People', peopleSchema)
+mongoose.connect(`mongodb://${dbUser}:${dbPassword}@ds137090.mlab.com:37090/spotify_challenge`)
+
+app.get('/people', (req, res) => {
+  People.find({}, (err, people) => {
+    if (err) console.log(err)
+
+    res.json(people)
+  })
+})
+
+app.post('/people', (req, res) => {
+  const body = req.body
+  // console.log('REQUEST BODY', body)
+  People.create({
+    name: body.name,
+    favoriteCity: body.favoriteCity,
+  }, (err, data) => {
+    if (err) console.log(err)
+
+    res.json(data)
+  })
+})
+
+// app.put('/people')
+
+// app.get('/people/:id')
+
+// app.delete('/people/:id')
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
